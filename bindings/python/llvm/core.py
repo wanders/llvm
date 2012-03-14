80d0fcec9e9409ec99b97824f4c5ec66af1385e8
Gregory Szorc <gregory.szorc@gmail.com>
2012-03-13 19:19:08 -0700

    [llvm.py] Move Module to own module; add bit writer APIs

--- bindings/python/llvm/core.py
+++ bindings/python/llvm/core.py
@@ -157,175 +157,6 @@ class MemoryBuffer(LLVMObject):
 
         LLVMObject.__init__(self, memory, disposer=lib.LLVMDisposeMemoryBuffer)
 
-class Module(LLVMObject):
-    """Represents an LLVM module.
-
-    Modules are an interface to the LLVM Intermediate Representation. They
-    contain global variables, functions, library/module dependencies, a symbol
-    table, and target characteristics.
-
-    See also llvm::Module::Module.
-
-    TODO support NamedMetadataOperands
-    """
-
-    def __init__(self, name=None, context=None):
-        """Construct a new module instance.
-
-        It is possible to create empty modules or to build modules from
-        existing sources. In all cases, you have the option of supplying an
-        llvm.core.Context to which to associate the module. If no context is
-        provided, the global context will be used.
-
-        If name is provided, an empty module with the specified str id will be
-        created.
-        """
-
-        if context is None:
-            context = Context(use_global=True)
-
-        if name is None:
-            raise Exception('You must supply a name to create a module.')
-
-        ptr = None
-
-        if name:
-            ptr = lib.LLVMModuleCreateWithNameInContext(name, context,
-                    disposer=lib.LLVMDisposeModule)
-
-        LLVMObject.__init__(self, ptr)
-        self._set_ref('context', context)
-
-    @property
-    def context(self):
-        """The Context to which this module is associated."""
-        return self._get_ref('context')
-
-    @property
-    def layout(self):
-        """The data layout string for this module's platform."""
-        return lib.LLVMGetDataLayout(self)
-
-    @layout.setter
-    def layout(self, value):
-        lib.LLVMSetDataLayout(self, value)
-
-    @property
-    def target(self):
-        """The target triple for this module."""
-        return lib.LLVMGetTarget(self)
-
-    @target.setter
-    def target(self, value):
-        lib.LLVMSetTarget(self, value)
-
-    @property
-    def inline_assembly(self):
-        # For whatever reason the C API doesn't expose this. We need to define
-        # the property so the setter works.
-        raise Exception('Not implemented.')
-
-    @inline_assembly.setter
-    def inline_assembly(self, value):
-        """Set the module-scope inline assembly blocks."""
-        lib.LLVMSetModuleInlineAsm(self, value)
-
-    def dump(self):
-        """Dump a representation of the module to stderr (for debugger)."""
-        lib.LLVMDumpModule(self)
-
-    def get_type(self, name):
-        """Return the Type with the specified name.
-
-        Returns a Type on success or None if no Type with that name exists.
-        """
-        assert isinstance(name, str)
-
-        ptr = lib.LLVMGetTypeByName(self, name)
-        if not ptr:
-            return None
-
-        return Type(ptr, module=self, context=self._get_ref('context'))
-
-    def add_global(self, ty, name, address_space=None):
-        """Add a global Type to the Module with the specified name.
-
-        Returns a Value.
-        """
-        assert isinstance(ty, Type)
-        assert isinstance(name, str)
-
-        ptr = None
-        if address_space:
-            ptr = lib.LLVMAddGlobalInAddressSpace(self, ty, name, address_space)
-        else:
-            ptr = lib.LLVMAddGlobal(self, ty, name)
-
-        return Value(ptr, module=self)
-
-    def get_global(self, name):
-        """Get a global Value by name."""
-
-        ptr = lib.LLVMGetNamedGlobal(self, name)
-        return Value(ptr, module=self)
-
-    def get_globals(self):
-        """Get all globals in this module.
-
-        This is a generator of Value instances.
-        """
-
-        ptr = lib.LLVMGetFirstGlobal(self)
-        g = None
-        while ptr:
-            g = Value(ptr, module=self)
-            yield g
-
-            ptr = lib.LLVMGetNextGlobal(g)
-
-    def delete_global(self, value):
-        """Delete a global Value from this Module."""
-        lib.LLVMDeleteGlobal(value)
-
-    def add_alias(self, ty, alias, name):
-        raise Exception('TODO implement.')
-
-    def add_function(self, func, name):
-        """Add a function to this Module under a specified name."""
-
-        assert isinstance(func, FunctionType)
-        assert isinstance(name, str)
-
-        ptr = lib.LLVMAddFunction(self, name, func)
-        return Value(ptr, module=self)
-
-    def get_function(self, name):
-        """Get a function by its str name."""
-
-        assert isinstance(name, str)
-
-        ptr = lib.LLVMGetNamedFunction(self, name)
-        return Value(ptr, module=self)
-
-    def get_functions(self):
-        """Obtain all functions in this Module.
-
-        This is a generator for Value instances.
-        """
-        ptr = lib.LLVMGetFirstFunction(self)
-        f = None
-        while ptr:
-            f = Value(ptr, module=self)
-            yield f
-
-            ptr = lib.LLVMGetNextFunction(f)
-
-    def delete_function(self, value):
-        """Delete a function Value from this Module."""
-        assert isinstance(value, Value)
-
-        lib.LLVMDeletionFunction(value)
-
 class TypeFactory(LLVMObject):
     """A factory for Type instances.
 
@@ -700,23 +531,9 @@ def register_library(library):
     This list is pretty long. We define APIs in alphabetical order for
     sanity.
     """
-
-    library.LLVMAddAlias.argtypes = [Module, Type, Value, c_char_p]
-    library.LLVMAddAlias.restype = c_object_p
-
     library.LLVMAddAttribute.argtypes = [ArgumentValue, Attribute]
 
     library.LLVMAddFunctionAttr.argtypes = [FunctionValue, Attribute]
-
-    library.LLVMAddFunction.argtypes = [Module, c_char_p, FunctionType]
-    library.LLVMAddFunction.restype = c_object_p
-
-    library.LLVMAddGlobalInAddressSpace.argtypes = [Module, Type, c_char_p,
-            c_uint]
-    library.LLVMAddGlobalInAddressSpace.restype = c_object_p
-
-    library.LLVMAddGlobal.argtypes = [Module, Type, c_char_p]
-    library.LLVMAddGlobal.restype = c_object_p
 
     library.LLVMArrayType.argtypes = [Type, c_uint]
     library.LLVMArrayType.restype = c_object_p
@@ -741,12 +558,8 @@ def register_library(library):
 
     library.LLVMDisposeMemoryBuffer.argtypes = [MemoryBuffer]
 
-    library.LLVMDisposeModule.argtypes = [Module]
-
     library.LLVMDoubleTypeInContext.argtypes = [Context]
     library.LLVMDoubleTypeInContext.result = c_object_p
-
-    library.LLVMDumpModule.argtypes = [Module]
 
     library.LLVMFloatTypeInContext.argtypes = [Context]
     library.LLVMFloatTypeInContext.result = c_object_p
@@ -767,17 +580,8 @@ def register_library(library):
     library.LLVMGetAttribute.argtypes = [ArgumentValue]
     library.LLVMGetAttribute.restype = c_int
 
-    library.LLVMGetDataLayout.argtypes = [Module]
-    library.LLVMGetDataLayout.restype = c_char_p
-
     library.LLVMGetElementType.argtypes = [Type]
     library.LLVMGetElementType.restype = c_object_p
-
-    library.LLVMGetFirstFunction.argtypes = [Module]
-    library.LLVMGetFirstFunction.restype = c_object_p
-
-    library.LLVMGetFirstGlobal.argtypes = [Module]
-    library.LLVMGetFirstGlobal.restype = c_object_p
 
     library.LLVMGetFirstParam.argtypes = [FunctionValue]
     library.LLVMGetFirstParam.restype = c_object_p
@@ -805,12 +609,6 @@ def register_library(library):
     library.LLVMGetLinkage.argtypes = [GlobalValue]
     library.LLVMGetLinkage.restype = c_int
 
-    library.LLVMGetNamedFunction.argtypes = [Module, c_char_p]
-    library.LLVMGetNamedFunction.restype = c_object_p
-
-    library.LLVMGetNamedGlobal.argtypes = [Module, c_char_p]
-    library.LLVMGetNamedGlobal.restype = c_object_p
-
     library.LLVMGetNextFunction.argtypes = [Value]
     library.LLVMGetNextFunction.restype = c_object_p
 
@@ -836,15 +634,8 @@ def register_library(library):
     library.LLVMGetStructName.argtypes = [Type]
     library.LLVMGetStructName.restype = c_char_p
 
-    library.LLVMGetTarget.argtypes = [Module]
-    library.LLVMGetTarget.restype = c_char_p
-
-    library.LLVMGetTypeByName.argtypes = [Module, c_char_p]
-    library.LLVMGetTypeByName.restype = c_object_p
-
     library.LLVMGetTypeKind.argtypes = [Type]
     library.LLVMGetTypeKind.restype = c_int
-
 
     library.LLVMGetVectorSize.argtypes = [Type]
     library.LLVMGetVectorSize.restype = c_uint
@@ -894,9 +685,6 @@ def register_library(library):
     library.LLVMLabelTypeInContext.argtypes = [Context]
     library.LLVMLabelTypeInContext.result = c_object_p
 
-    library.LLVMModuleCreateWithNameInContext.argtypes = [c_char_p, Context]
-    library.LLVMModuleCreateWithNameInContext.restype = c_object_p
-
     library.LLVMPPCFP128TypeInContext.argtypes = [Context]
     library.LLVMPPCFP128TypeInContext.result = c_object_p
 
@@ -909,8 +697,6 @@ def register_library(library):
 
     library.LLVMSetAlignment.argtypes = [GlobalValue, c_uint]
 
-    library.LLVMSetDataLayout.argtypes = [Module, c_char_p]
-
     library.LLVMSetFunctionCallConv.argtypes = [FunctionValue, c_uint]
 
     library.LLVMSetGC.argtypes = [FunctionValue, c_char_p]
@@ -921,14 +707,9 @@ def register_library(library):
 
     library.LLVMSetLinkage.argtypes = [GlobalValue, c_int]
 
-    library.LLVMSetModuleInlineAsm.argtypes = [Module, c_char_p]
-
     library.LLVMSetSection.argtypes = [GlobalValue, c_char_p]
 
-    library.LLVMSetTarget.argtypes = [Module, c_char_p]
-
     library.LLVMSetThreadLocal.argtypes = [GlobalValue]
-
 
     library.LLVMSetVisibility.argtypes = [GlobalValue, c_int]
 
@@ -941,7 +722,6 @@ def register_library(library):
     library.LLVMStructTypeInContext.argtypes = [Context, POINTER(c_object_p),
             c_uint, c_int]
     library.LLVMStructTypeInContext.result = c_object_p
-
 
     library.LLVMTypeIsSized.argtypes = [Type]
     library.LLVMTypeIsSized.restype = c_bool
