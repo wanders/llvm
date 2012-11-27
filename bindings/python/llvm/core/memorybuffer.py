eb03ab9b3ae054c407b83f34a980ccd6f9a7f09c
Anders Waldenborg <anders@0x63.nu>
2012-11-27 22:15:36 +0100

    [python] Allow creating memorybuffer from contents

--- bindings/python/llvm/core/memorybuffer.py
+++ bindings/python/llvm/core/memorybuffer.py
@@ -14,20 +14,30 @@ from ..common import get_library
 from ctypes import byref
 from ctypes import c_char_p
 
+from tempfile import NamedTemporaryFile
+
 lib = get_library()
 
 @lib.c_name("LLVMMemoryBufferRef")
 class MemoryBuffer(LLVMObject):
     """Represents an opaque memory buffer."""
 
-    def __init__(self, filename=None):
+    def __init__(self, filename=None, contents=None):
         """Create a new memory buffer.
 
-        Currently, we support creating from the contents of a file at the
-        specified filename.
+        If `filename` argument is specified the memory buffer will
+        contain the contents of that file. If `contents`, which should
+        be a string, is specified the memory buffer will contain that
+        data.
         """
-        if filename is None:
-            raise Exception("filename argument must be defined")
+        if filename is None and contents is None:
+            raise TypeError("filename or contents argument must be defined")
+
+        if contents is not None:
+            tmp = NamedTemporaryFile()
+            tmp.write(contents)
+            tmp.flush()
+            filename = tmp.name
 
         memory = c_object_p()
         out = c_char_p(None)
@@ -36,7 +46,7 @@ class MemoryBuffer(LLVMObject):
                 byref(memory), byref(out))
 
         if result:
-            raise Exception("Could not create memory buffer: %s" % out.value)
+            raise RuntimeError("Could not create memory buffer: %s" % out.value)
 
         LLVMObject.__init__(self, memory)
 
