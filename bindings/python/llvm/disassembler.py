2e8ad17232e2b605394158d1bdc7237070c4c0cc
Anders Waldenborg <anders@0x63.nu>
2013-10-07 21:39:30 +0200

    python: Properly initialize before trying to create disasm

--- bindings/python/llvm/disassembler.py
+++ bindings/python/llvm/disassembler.py
@@ -33,6 +33,16 @@ callbacks = {}
 # Constants for set_options
 Option_UseMarkup = 1
 
+_initialized = False
+def _ensure_initialized():
+    global _initialized
+    if not _initialized:
+        lib.LLVMInitializeAllTargetInfos()
+        lib.LLVMInitializeAllTargetMCs()
+        lib.LLVMInitializeAllDisassemblers()
+        _initialized = True
+
+
 class Disassembler(LLVMObject):
     """Represents a disassembler instance.
 
@@ -47,6 +57,9 @@ class Disassembler(LLVMObject):
         The triple argument is the triple to create the disassembler for. This
         is something like 'i386-apple-darwin9'.
         """
+
+        _ensure_initialized()
+
         ptr = lib.LLVMCreateDisasm(c_char_p(triple), c_void_p(None), c_int(0),
                 callbacks['op_info'](0), callbacks['symbol_lookup'](0))
         if not ptr:
